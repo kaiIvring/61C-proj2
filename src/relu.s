dcae5e0fdd9a212aca139123c88ebb5dc19684d3
KaiIvring <3011973335@qq.com>
2025-04-13 11:01:56 +0800

    abs and relu finished

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,30 @@
 # ==============================================================================
 relu:
     # Prologue
-
-
+    li t0, 1
+    blt a1, t0, error_code36
+    
+    mv t1, a0 # t1 = base address
+    mv t2, a1 # t2 = counter
+    
 loop_start:
-
-
-
-
-
-
-
+    beq t2, x0, loop_end
+    
+    lw t3, 0(t1)
+    bge t3, x0, loop_continue
+    sw x0, 0(t1)
 
 loop_continue:
-
-
+    addi t1, t1, 4
+    addi t2, t2, -1
+    j loop_start
 
 loop_end:
 
-
     # Epilogue
 
-
     jr ra
+
+error_code36:
+    li a0, 36
+    j exit
